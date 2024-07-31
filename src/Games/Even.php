58fed52ae5f4c1641dada1e47380f0e2633a473d
semyanskikh-im <semyanskikh@gmail.com>
2024-07-31 20:00:34 +0300

    modified Even.php for use fuction Engine

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -4,37 +4,27 @@ namespace BrainGames\Games\Even;
 
 use function cli\line;
 use function cli\prompt;
+use function BrainGames\Games\Engine\engine;
 
-function runEven()
+function runEven() 
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $description = 'Answer "yes" if the number is even, otherwise answer "no"';
 
-    $i = 0;
-    while ($i <= 2) {
+    $questions = []; // пустой массив для вопросов
+    $answers = [];  //пустой массив для ответов
 
-        $number = rand(1, 100); // генерируем случайное число от 1 до 100
-    line("Question: %d", $number);
-    $userAnswer = prompt('Your answer'); // игрок дает ответ
+    for ($i = 0; $i < 3; $i++) {
 
-    if ($number % 2 === 0) {             // правильный ответ
-        $correctAnswer = "yes";
+    $number = rand(1, 100); //по очереди генерируем вопрос
+    $questions[] = $number; // кладем в массив
+        
+    if ($number % 2 === 0) {
+        $correctAnswer = "yes";   //генерируем ответ на вопрос выше
     } else {
         $correctAnswer = "no";
     }
-
-    if ($userAnswer === $correctAnswer) {
-        line('Correct!');                   //игрок дал правильный ответ
-        $i++;                               // счетчик увеличился на 1
-    } else {
-        echo "'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.\nLet's try again, {$name}!\n";
-        $i = 5;                      //игрок дал неверный ответ. Счетчик равен 5. Выход из цикла.
-    } 
+    $answers[] = $correctAnswer; // кладем ответ в массив
     }
 
-    if ($i === 3) {
-        echo "Congratulations, {$name}!\n"; //если счетчик равен 3, т.е. 3 правильных ответа подряд - поздравляем
-    }
+    engine($description, $questions, $answers); //передаем в функцию движка
 }
